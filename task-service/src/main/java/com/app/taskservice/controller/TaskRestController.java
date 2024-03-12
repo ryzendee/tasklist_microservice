cd8192671a57cd53e75ba74318742630467b7bcb
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-12 17:55:03 +0400

    feat: Implemented task rest controller

---
+++ task-service/src/main/java/com/app/taskservice/controller/TaskRestController.java
@@ -0,0 +1,41 @@
+package com.app.taskservice.controller;
+
+import com.app.taskservice.dto.request.CreateTaskRequest;
+import com.app.taskservice.dto.response.TaskResponse;
+import com.app.taskservice.entity.task.TaskEntity;
+import com.app.taskservice.mapper.task.TaskEntityResponseMapper;
+import com.app.taskservice.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
+public class TaskRestController {
+
+    private final TaskService taskService;
+    private final TaskEntityResponseMapper taskEntityResponseMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskResponse createTask(@Valid @RequestBody CreateTaskRequest createTaskRequest) {
+        TaskEntity task =  taskService.createTask(createTaskRequest);
+        return taskEntityResponseMapper.toDto(task);
+    }
+
+    @PutMapping("/{taskId}")
+    public TaskResponse updateTaskStatusById(@PathVariable Long taskId,
+                                             @RequestParam String status) {
+
+        TaskEntity task = taskService.updateTaskStatus(taskId, status);
+        return taskEntityResponseMapper.toDto(task);
+    }
+
+    @DeleteMapping("/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTaskById(@PathVariable Long taskId) {
+        taskService.deleteTask(taskId);
+    }
+}
