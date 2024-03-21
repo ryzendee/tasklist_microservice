bc9e20d37db825dae8fc2105aa998370e2a08f6a
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-21 16:30:29 +0400

    feat: Implemented auth controller

---
+++ auth-service/src/main/java/com/app/authservice/controller/AuthRestController.java
@@ -0,0 +1,53 @@
+package com.app.authservice.controller;
+
+import com.app.authservice.dto.request.*;
+import com.app.authservice.dto.response.*;
+import com.app.authservice.entity.AuthUser;
+import com.app.authservice.service.authuser.AuthUserService;
+import com.app.authservice.service.jwt.JwtService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+
+    private static final String AUTH_HEADER = "Authorization";
+
+    private final AuthUserService authUserService;
+    private final JwtService jwtService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse signupUser(@Valid @RequestBody SignupRequest signupRequest) {
+        AuthUser createdUser = authUserService.createuser(signupRequest);
+        return getTokenResponse(createdUser);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse loginUser(@Valid @RequestBody LoginRequest loginRequest) {
+        AuthUser user = authUserService.loginUser(loginRequest);
+        return getTokenResponse(user);
+    }
+
+    private TokenResponse getTokenResponse(AuthUser user) {
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
+    @PostMapping("/refresh")
+    public TokenResponse refreshAccessToken(@RequestHeader(AUTH_HEADER) String refreshToken) {
+        String accessToken = jwtService.refreshAccessToken(refreshToken);
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
+    @PostMapping("/retrieveToken")
+    public void retrieveRefreshToken(@RequestHeader(AUTH_HEADER) String refreshToken) {
+        jwtService.retrieveRefreshToken(refreshToken);
+    }
+}
