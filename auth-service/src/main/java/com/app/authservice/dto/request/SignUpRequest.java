f569841f5ad900614906359e745f29164459b870
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-25 19:12:19 +0400

    feat: Added validation for signup request

--- auth-service/src/main/java/com/app/authservice/dto/request/SignUpRequest.java
+++ auth-service/src/main/java/com/app/authservice/dto/request/SignUpRequest.java
@@ -1,8 +1,17 @@
 package com.app.authservice.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record SignUpRequest(
+        @Email(message = "Incorrect email format")
         String email,
+        @NotNull(message = "Password must not be null")
+        @NotBlank(message = "Password must not be blank")
         String password,
+        @NotNull(message = "Password confirmation must not be null")
+        @NotBlank(message = "Password confirmation must not be blank")
         String passwordConfirmation
 ) {
 }
