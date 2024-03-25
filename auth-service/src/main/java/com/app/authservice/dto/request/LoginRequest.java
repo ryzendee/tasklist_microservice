4e09907d3b20273c350abf862126a67d7ad417ac
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-25 19:32:50 +0400

    feat: Added validation for login request

--- auth-service/src/main/java/com/app/authservice/dto/request/LoginRequest.java
+++ auth-service/src/main/java/com/app/authservice/dto/request/LoginRequest.java
@@ -1,7 +1,16 @@
 package com.app.authservice.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record LoginRequest(
+        @NotNull(message = "Email must not be null")
+        @Email(message = "Incorrect email-format")
         String email,
+
+        @NotNull(message = "Password must not be null")
+        @NotBlank(message = "Password must not be blank")
         String password
 ) {
 }
