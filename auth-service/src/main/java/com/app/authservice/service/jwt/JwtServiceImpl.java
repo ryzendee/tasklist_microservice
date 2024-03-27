eb96dc0df40ff8641b4b4320a67ea7f4575a33bf
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-03-27 15:07:11 +0400

    refactor: Replaced token provider, changed code structure

--- auth-service/src/main/java/com/app/authservice/service/jwt/JwtServiceImpl.java
+++ auth-service/src/main/java/com/app/authservice/service/jwt/JwtServiceImpl.java
@@ -3,21 +3,28 @@ package com.app.authservice.service.jwt;
 import com.app.authservice.entity.AuthUser;
 import com.app.authservice.enums.TokenType;
 import com.app.authservice.exception.custom.GenerateTokenException;
-import com.app.authservice.mapper.interfaces.AuthUserMapToTokenData;
-import com.app.authservice.models.TokenData;
-import com.app.authservice.utils.jwt.provider.JwtTokenProvider;
+import com.app.authservice.repository.token.TokenRepository;
+import com.app.authservice.utils.jwt.factory.JwtFactory;
+import com.app.authservice.utils.jwt.generator.JwtClaimsCreator;
+import com.app.authservice.utils.jwt.parser.JwtParser;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class JwtServiceImpl implements JwtService {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AuthUserMapToTokenData authUserMapToTokenData;
+    private final TokenRepository tokenRepository;
+
+    private final JwtFactory jwtFactory;
+    private final JwtParser jwtParser;
+    private final JwtClaimsCreator jwtClaimsCreator;
 
     @Override
     public String generateAccessToken(AuthUser authUser) throws GenerateTokenException {
@@ -30,34 +37,60 @@ public class JwtServiceImpl implements JwtService {
     }
 
     private String generateToken(AuthUser authUser, TokenType tokenType) {
-        TokenData tokenData = authUserMapToTokenData.map(authUser);
-        try {
-            return tokenType.equals(TokenType.REFRESH)
-                    ? jwtTokenProvider.generateRefreshToken(tokenData)
-                    : jwtTokenProvider.generateAccessToken(tokenData);
-        } catch (JwtException ex) {
-            log.error("Failed to generate token: {}", ex.getMessage());
-            throw new GenerateTokenException("Failed to generate token");
+        validateUser(authUser);
+        Claims claims = jwtClaimsCreator.getTokenClaims(authUser, tokenType);
+        return jwtFactory.createToken(claims);
+    }
+
+
+    private void validateUser(AuthUser authUser) {
+        if (authUser.getId() == null || authUser.getRole() == null) {
+            log.error("Invalid user credentials: id={}, role={}", authUser.getId(), authUser.getRole());
+            throw new GenerateTokenException("Invalid user credentials");
         }
     }
 
     @Override
     public String refreshAccessToken(String refreshToken) {
-        try {
-            return jwtTokenProvider.refreshAccessToken(refreshToken);
-        } catch (JwtException ex) {
-            log.error("Failed to refresh access token: {}", ex.getMessage());
-            throw new GenerateTokenException("Failed to refresh access token");
-        }
+        validateToken(refreshToken);
+
+        Claims claimsFromRefreshToken = jwtParser.parseToken(refreshToken);
+        Claims updatedClaims = jwtClaimsCreator.refreshClaimsForAccessToken(claimsFromRefreshToken);
+
+        return jwtFactory.createToken(updatedClaims);
     }
 
     @Override
     public void retrieveRefreshToken(String refreshToken) {
-        //TODO
+        validateToken(refreshToken);
+        Claims claims = jwtParser.parseToken(refreshToken);
+        tokenRepository.saveTokenWithExpiration(claims.getId(), refreshToken, claims.getExpiration().getTime());
+    }
+
+    private void validateToken(String token) {
+        if (!isTokenValid(token) || isTokenRetrieved(token)) {
+            throw new JwtException("Invalid or retrieved token!");
+        }
     }
 
     @Override
     public boolean isTokenValid(String token) {
-        return jwtTokenProvider.isTokenValid(token);
+        if (isTokenRetrieved(token)) {
+            return false;
+        }
+
+        try {
+            Claims claims = jwtParser.parseToken(token);
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException ex) {
+            log.error("Failed to parse token: {}", ex.getMessage());
+            return false;
+        }
+    }
+
+    private boolean isTokenRetrieved(String token) {
+        Claims claims = jwtParser.parseToken(token);
+        return tokenRepository.findTokenById(claims.getId())
+                .isPresent();
     }
 }
