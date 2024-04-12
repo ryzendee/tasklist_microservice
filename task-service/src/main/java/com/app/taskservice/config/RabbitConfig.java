9ea3ed8dff0655091bbbc1e956d9dd6fc5fb41bc
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-12 16:38:13 +0400

    feat: Added beans for rabbitTemplate

--- task-service/src/main/java/com/app/taskservice/config/RabbitConfig.java
+++ task-service/src/main/java/com/app/taskservice/config/RabbitConfig.java
@@ -5,6 +5,11 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,5 +39,26 @@ public class RabbitConfig {
     @Bean
     public Binding bindingTaskReportEmailQueue(Queue taskReportEmailQueue, DirectExchange emailExchangeName) {
         return BindingBuilder.bind(taskReportEmailQueue).to(emailExchangeName).with(taskReportEmailRoutingKey);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory();
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+
+        return rabbitTemplate;
     }
 }
