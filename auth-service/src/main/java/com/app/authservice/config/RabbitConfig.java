5fdf020c866fd4ecd107e744f49461092a8b04bb
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-12 13:16:43 +0400

    feat: Added beans for queues in config

--- auth-service/src/main/java/com/app/authservice/config/RabbitConfig.java
+++ auth-service/src/main/java/com/app/authservice/config/RabbitConfig.java
@@ -1,10 +1,18 @@
 package com.app.authservice.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +20,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableRabbit
 public class RabbitConfig {
 
+
+    @Value("${email-exchange.name}")
+    private String emailExchangeName;
+    @Value("${email-exchange.queues.registration.name}")
+    private String userEmailQueueName;
+    @Value("${email-exchange.queues.registration.routing}")
+    private String userEmailRoutingKey;
 
     @Bean
     public ConnectionFactory connectionFactory() {
@@ -30,6 +45,21 @@ public class RabbitConfig {
         rabbitTemplate.setConnectionFactory(factory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public DirectExchange emailExchange() {
+        return new DirectExchange(emailExchangeName);
+    }
+
+    @Bean
+    public Queue userEmailQueue() {
+        return new Queue(userEmailQueueName);
+    }
+
+    @Bean
+    public Binding bindingUserEmailQueue(Queue userEmailQueue, DirectExchange emailExchange) {
+        return BindingBuilder.bind(userEmailQueue).to(emailExchange).with(userEmailRoutingKey);
     }
 
 }
