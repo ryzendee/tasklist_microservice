4d8b8127373e8b44316b5c534229a6d536100367
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-15 17:42:40 +0400

    feat: Added before saving not null validation

--- task-service/src/main/java/com/app/taskservice/entity/task/TaskEntity.java
+++ task-service/src/main/java/com/app/taskservice/entity/task/TaskEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
 
 @Data
 @AllArgsConstructor
@@ -15,17 +17,23 @@ public class TaskEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "userId must not be null")
+    private Long userId;
+    @NotNull(message = "title must not be null")
     private String title;
+    @Lob
     private String description;
     private TaskStatus status;
 
-    public TaskEntity(String title, String description, TaskStatus status) {
+    public TaskEntity(Long userId, String title, String description, TaskStatus status) {
+        this.userId = userId;
         this.title = title;
         this.description = description;
         this.status = status;
     }
 
-    public TaskEntity(String title, String description) {
+    public TaskEntity(Long userId, String title, String description) {
+        this.userId = userId;
         this.title = title;
         this.description = description;
     }
