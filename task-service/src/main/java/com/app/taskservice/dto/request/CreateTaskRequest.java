c008a5ae272c7b78da40def47d870a9109e33997
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-15 17:33:34 +0400

    fix: Added userId

--- task-service/src/main/java/com/app/taskservice/dto/request/CreateTaskRequest.java
+++ task-service/src/main/java/com/app/taskservice/dto/request/CreateTaskRequest.java
@@ -1,11 +1,16 @@
 package com.app.taskservice.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreateTaskRequest (
+
+        @NotNull(message = "User id must not be null")
+        @Min(value = 0, message = "User id must be positive value")
+        Long userId,
         @NotNull(message = "Task title must not be null")
         @NotBlank(message = "Task title must not be blank")
         String title,
--- task-service/src/main/java/com/app/taskservice/dto/response/TaskResponse.java
+++ task-service/src/main/java/com/app/taskservice/dto/response/TaskResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 @Builder
 public record TaskResponse (
         Long id,
+        Long userId,
         String title,
         String description,
         String status
