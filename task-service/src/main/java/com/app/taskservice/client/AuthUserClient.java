930ea4f8e1bd3d5bb9d2d9de28835737654a2cac
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-15 17:32:32 +0400

    refactor: Removed url

--- task-service/src/main/java/com/app/taskservice/client/AuthUserClient.java
+++ task-service/src/main/java/com/app/taskservice/client/AuthUserClient.java
@@ -1,16 +1,16 @@
 package com.app.taskservice.client;
 
-import com.app.taskservice.dto.response.UserResponse;
+import com.app.taskservice.dto.response.AuthUserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "auth-service", url = "localhost:8989")
+@FeignClient(name = "auth-service")
 public interface AuthUserClient {
 
-    @GetMapping
-    ResponseEntity<Page<UserResponse>> getAllUsers(@RequestParam int page,
-                                                   @RequestParam int size);
+    @GetMapping("/api/v1/auth")
+    Page<AuthUserResponse> getAllUsers(@RequestParam int page,
+                                                       @RequestParam int size);
 }
